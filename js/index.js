c2bce535a60964c71c8a2afceea3d83b943e3525
GuillaumeMetz <guillaumemetz68@gmail.com>
2024-03-17 19:42:28 +0100

    test js controller

--- js/index.js
+++ js/index.js
@@ -18,3 +18,24 @@
             arrow.setAttribute('visible', 'false');
         }
     });
+
+    AFRAME.registerComponent('change-color-on-click', {
+        // Could use a schem to preserve the color! then simply change it on update
+        // if clicked?
+        init: function () {
+            var COLORS = [
+                'pink',
+                //'blue',
+                'yellow',
+                'red',
+                'peachpuff',
+                '#2EAFAC',
+                '#BAE'];
+            this.el.addEventListener('click', function (evt) {
+                var randomIndex = Math.floor(Math.random() * COLORS.length);
+                var newColor = COLORS[randomIndex];
+                this.setAttribute('material', 'color', newColor);
+                console.log('I was clicked at: ', evt.detail.intersection.point, "and my new color is: ", newColor);
+            });
+        }
+    });
